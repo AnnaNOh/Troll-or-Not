1eb35222ad25a4964713f23ed25df1f70df2db27
Anna <annaoh1994@gmail.com>
2018-05-10 19:43:02 -0700

    database changes for User table

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,50 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING
-  }, {});
+  var User = sequelize.define(
+    'User',
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      img_url: {
+        type: DataTypes.STRING
+      },
+      google_password_digest: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      facebook_password_digest: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email_password_digest: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      session_token: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      commentator_score: {
+        type: DataTypes.STRING
+      },
+      eagle_eye_score: {
+        type: DataTypes.STRING
+      },
+      binger_score: {
+        type: DataTypes.STRING
+      },
+      troll_hunter_score: {
+        type: DataTypes.STRING
+      }
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
